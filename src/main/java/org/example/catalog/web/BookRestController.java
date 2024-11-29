0af9e5d521af572679d9423d09d83adea4c29509
Furkan <furkan.aydin@students.fhnw.ch>
2024-11-29 10:13:08 +0100

    getBookById methode hinzugefügt

--- src/main/java/org/example/catalog/web/BookRestController.java
+++ src/main/java/org/example/catalog/web/BookRestController.java
@@ -3,12 +3,11 @@ package org.example.catalog.web;
 import org.example.catalog.Book;
 import org.example.catalog.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -26,6 +25,11 @@ public class BookRestController {
             return bookRepository.findAll(); // Alle Bücher zurückgeben
         }
         return bookRepository.searchByKeywords(keywords);
+    }
+
+    @GetMapping("/{isbn}")
+    public Optional<Book> getBookById(@PathVariable String isbn) {
+        return bookRepository.findById(isbn);
     }
 }
 
