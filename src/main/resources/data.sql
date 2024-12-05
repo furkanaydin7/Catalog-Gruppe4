bd1c1868a3f8a720f7a3570943ad7597e42f9a25
Furkan <furkan.aydin@students.fhnw.ch>
2024-12-05 19:23:12 +0100

    SQL datei hinzugefügt mit PostgreSQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (isbn, title, description, author) VALUES
+    ('9780596520687', 'Java Programming', 'An introduction to programming in Java.', 'Kathy Sierra'),
+('9783161484100', 'Spring Boot in Action', 'A comprehensive guide to Spring Boot.', 'Craig Walls'),
+('9781491924350', 'Learning Spring Boot', 'Simplify your development with Spring Boot.', 'Greg L. Turnquist');
